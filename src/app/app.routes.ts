f38be6371a9fab95dd28b705e814e5a6424f7883
ppvanh05 <anhppv23406@st.uel.edu.vn>
2026-06-20 09:40:11 +0700

    Cập nhật giao diện admin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { AdminLayoutComponent } from './core/layout/admin-layout/admin-layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'admin',
+    component: AdminLayoutComponent,
+    children: [
+      {
+        path: 'trangchu',
+        loadComponent: () =>
+          import('./featured/admin/trangchu/trangchu').then(
+            (m) => m.TrangChuComponent
+          ),
+      },
+      {
+        // Redirect /admin → /admin/trangchu
+        path: '',
+        redirectTo: 'trangchu',
+        pathMatch: 'full',
+      },
+    ],
+  },
+  {
+    // Redirect root → /admin/trangchu
+    path: '',
+    redirectTo: 'admin/trangchu',
+    pathMatch: 'full',
+  },
+];
