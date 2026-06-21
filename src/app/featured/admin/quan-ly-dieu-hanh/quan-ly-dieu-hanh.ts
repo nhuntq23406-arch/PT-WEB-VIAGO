e26b9c080dfd415739dc85181bf24a9fe19c9e87
PhuongDo186 <phuongdt23406@st.uel.edu.vn>
2026-06-21 20:45:48 +0700

    feat: import admin module from admin branch

---
+++ src/app/featured/admin/quan-ly-dieu-hanh/quan-ly-dieu-hanh.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quan-ly-dieu-hanh',
+  imports: [],
+  templateUrl: './quan-ly-dieu-hanh.html',
+  styleUrl: './quan-ly-dieu-hanh.css',
+})
+export class QuanLyDieuHanh {}
---
+++ src/app/featured/admin/quan-ly-ve/quan-ly-ve.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quan-ly-ve',
+  imports: [],
+  templateUrl: './quan-ly-ve.html',
+  styleUrl: './quan-ly-ve.css',
+})
+export class QuanLyVe {}
