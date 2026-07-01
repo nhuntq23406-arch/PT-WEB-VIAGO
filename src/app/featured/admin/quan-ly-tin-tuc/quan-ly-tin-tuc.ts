76d511b18abd38d08b84d920618cd8b4a6cc6142
Quynh Nhu <nhuntq23406@st.uel.edu.vn>
2026-07-01 13:15:27 +0700

    Remove unused admin modules

--- src/app/featured/admin/quan-ly-tin-tuc/quan-ly-tin-tuc.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-quan-ly-tin-tuc',
-  imports: [],
-  templateUrl: './quan-ly-tin-tuc.html',
-  styleUrl: './quan-ly-tin-tuc.css',
-})
-export class QuanLyTinTuc {}
